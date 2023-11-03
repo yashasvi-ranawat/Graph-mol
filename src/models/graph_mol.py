8d4568c6ae9390105ce25a902480deda5e7eab78
Yashasvi S. Ranawat <yashasvi.ranawat@gmail.com>
2023-11-03 12:06:49 +0200

    adds training template

---
+++ src/models/graph_mol.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+
+class GraphMol(nn.Module):
+    def __init__(
+        self,
+    ):
+        super(GraphMol, self).__init()
+        pass
+
+    def forward(self, x, egde_index, edge_attr):
+        pass
---
+++ src/training.py
@@ -0,0 +1,50 @@
+import os
+
+import torch
+from data.datamodule import DataModule
+from models.graph_mol import GraphModel
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import (
+    LearningRateMonitor,
+    ModelCheckpoint,
+    TQDMProgressBar,
+)
+
+
+def main():
+    # Select the device for training (use GPU if you have one)
+    CHECKPOINT_PATH = "ckpts"
+    MODEL_PATH = "app"
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+
+    data_module = DataModule(batch_size=32)
+
+    # Create a PyTorch Lightning trainer with the generation callback
+    trainer = pl.Trainer(
+        default_root_dir=os.path.join(CHECKPOINT_PATH, "model"),
+        gpus=1 if str(device).startswith("cuda") else 0,
+        max_epochs=60,
+        gradient_clip_val=0.1,
+        callbacks=[
+            ModelCheckpoint(
+                save_weights_only=True, mode="min", monitor="val_contrastive_divergence"
+            ),
+            LearningRateMonitor("epoch"),
+            TQDMProgressBar(),
+        ],
+    )
+    # Check whether pretrained model exists. If yes, load it
+    pretrained_filename = os.path.join(MODEL_PATH, "model.ckpt")
+    if os.path.isfile(pretrained_filename):
+        print("Found pretrained model, loading...")
+        model = GraphModel.load_from_checkpoint(pretrained_filename)
+    else:
+        pl.seed_everything(142)
+        model = GraphModel()
+
+    trainer.fit(model, data_module)
+
+
+if __name__ == "__main__":
+    main()
